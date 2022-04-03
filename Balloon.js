02b4bcc8f20be8ff98bff502c7192d74b8660e27
Galactyce <elias.c.geurts@gmail.com>
2022-04-03 17:57:34 -0500

    Commit

--- Balloon.js
+++ Balloon.js
@@ -1,36 +1,36 @@
 function Balloon(xPosition, velocity, index, health) {
   this.position =  new Vector2(xPosition, -100);
   this.rainbowBalloonChance = 2.7;
-  this.index = index
+  this.index = index;
   this.health = health;
-  this.currentColor = undefined
-  this.timePopped = undefined
-  this.positionPopped = undefined
+  this.currentColor = undefined;
+  this.timePopped = undefined;
+  this.positionPopped = undefined;
   this.popPointValue = 10;
   this.popHeartValue = 1;
-  this.velocity = new Vector2(0, velocity)
+  this.velocity = new Vector2(0, velocity);
   this.origin = new Vector2(60, 60);
   this.minVelocity = 30 + Game.gameWorld.score / 20;
   this.wavy = false;
   this.moveDir = -2;
   this.leftMoved = false;
-  this.rightMoved = false
+  this.rightMoved = false;
   this.rainbowProbability = 0.06;
   this.specialProbability = 0.035 + Game.gameWorld.specialBalloons.length / 200 + Game.gameWorld.score / 25000;
-  this.penaltyProbability = 0.035 + Game.gameWorld.penaltyBalloons.length / 200 + Game.gameWorld.score / 25000
+  this.penaltyProbability = 0.035 + Game.gameWorld.penaltyBalloons.length / 200 + Game.gameWorld.score / 25000;
   
   this.chooseColor();
-  this.moveToTop()
-  this.calculateRandomVelocity()
-  this.applyHealth()
+  this.moveToTop();
+  this.calculateRandomVelocity();
+  this.applyHealth();
 }
 
 
 
 Balloon.prototype.applyHealth = function() {
   if (this.currentColor === 'metal') {
-    this.health = 9
-    this.velocity.y = 25
+    this.health = 9;
+    this.velocity.y = 25;
   }
   
 }
@@ -41,13 +41,12 @@ Balloon.prototype.moveToTop = function() {
 }
 
 Balloon.prototype.draw = function() {
-   Canvas.drawImage(sprites.balloons[this.currentColor].normal, this.position, 0, this.origin)
+   Canvas.drawImage(sprites.balloons[this.currentColor].normal, this.position, 0, this.origin);
 }
 
 Balloon.prototype.calculateRandomVelocity = function() {
-  this.velocity.y =  Math.random() * 20 + this.minVelocity
+  this.velocity.y =  Math.random() * 20 + this.minVelocity;
 }
-
 
 
 
@@ -58,13 +57,13 @@ Balloon.prototype.update = function (delta) { // 1
       if (this.position.x >= 1100 && this.leftMoved === false) {
         this.leftMoved = true;
         this.rightMoved = false;
-        this.moveDir = -1
+        this.moveDir = -1;
 
       } 
       if (this.position.x <= 700 && this.rightMoved === false) {
         this.rightMoved = true;
         this.leftMoved = false;
-        this.moveDir = 1
+        this.moveDir = 1;
 
       }
     this.position.x += this.moveDir;
