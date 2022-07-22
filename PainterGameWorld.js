e6bdffeb04dd9ac2fb0db4a1f7aae5df56dc4177
Galactyce <elias.c.geurts@gmail.com>
2022-07-22 12:20:20 -0500

    adfasdf

--- PainterGameWorld.js
+++ PainterGameWorld.js
@@ -452,6 +452,7 @@ GameWorld.prototype.updateCookies = function() {
   for (var i=0; i<this.specialtiesOwned.length; i++) {
     document.cookie = 'item' + [i] + "=" + this.specialtiesOwned[i] + ';';
   }
+    document.cookie = 'coins=' + this.coins + ';'
   console.log(document.cookie)
 
 }
