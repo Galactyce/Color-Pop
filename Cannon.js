df6363cdaa42b9bdcc1db3886b008fc507a164b5
Galactyce <elias.c.geurts@gmail.com>
2022-07-23 18:26:17 -0500

    Commit

--- Cannon.js
+++ Cannon.js
@@ -54,9 +54,9 @@ Cannon.prototype.handleInput = function () {
     if (Touch.touches.length > 0) {
     var opp = Touch.getPosition(0).y - this.position.y;
     var adj = Touch.getPosition(0).x - this.position.x;
+    }
     this.rotation = Math.atan2(opp, adj);
 
-    }
   }
     if (Mouse.pressed && Game.gameWorld.started) {
       this.shoot()        
