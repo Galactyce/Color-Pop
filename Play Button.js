cc88f842dff39407eb7cea60c32a6ac692ac7993
Galactyce <elias.c.geurts@gmail.com>
2022-07-23 16:51:17 -0500

    Commit

--- Play Button.js
+++ Play Button.js
@@ -37,7 +37,6 @@ PlayButton.prototype.update = function () {
   if (Touch.isTouchDevice && Touch.containsTouch(this.rect) &&
   Game.gameWorld.gameActive === false 
   ) {
-    alert()
     this.start()
   }
   else if (
