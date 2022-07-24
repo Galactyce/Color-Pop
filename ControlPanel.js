769a4c0905da40eff90734910952fa9fcaff37a4
Galactyce <elias.c.geurts@gmail.com>
2022-07-24 11:14:18 -0500

    Commit

--- ControlPanel.js
+++ ControlPanel.js
@@ -5,14 +5,14 @@ function ControlPanel() {
   var mx = (50 - canvasOffset.x) * canvasScale.x;
   var my = (300 - canvasOffset.y) * canvasScale.y;
 
-  this.rect = new Rectangle(mx, my, 300 * canvasScale.x, 150 * canvasScale.y)
+  this.rect = new Rectangle(mx, my, 300 * canvasScale.x, 190 * canvasScale.y)
 }
 
 ControlPanel.prototype.draw = function() {
   var canvasScale = Canvas.scale;
   var canvasOffset = Canvas.offset;
-  var mx = (50 - canvasOffset.x) * canvasScale.x;
-  var my = (300 - canvasOffset.y) * canvasScale.y;
+  var mx = (50) * canvasScale.x;
+  var my = (300) * canvasScale.y;
 
   Canvas.context.fillStyle = 'red';
   Canvas.context.fillRect(mx, my, 300 * canvasScale.x, 150 * canvasScale.y)
