01d010f1afa76a81433d562320551a8280b62e6e
Galactyce <elias.c.geurts@gmail.com>
2022-07-24 10:56:05 -0500

    gsdf

--- ControlPanel.js
+++ ControlPanel.js
@@ -1,13 +1,19 @@
-var canvasScale = Canvas.scale;
+
+function ControlPanel() {
+  var canvasScale = Canvas.scale;
   var canvasOffset = Canvas.offset;
   var mx = (50 - canvasOffset.x) / canvasScale.x;
   var my = (300 - canvasOffset.y) / canvasScale.y;
 
-function ControlPanel() {
   this.rect = new Rectangle(mx, my, 300 * canvasScale.x, 150 * canvasScale.y)
 }
 
 ControlPanel.prototype.draw = function() {
+  var canvasScale = Canvas.scale;
+  var canvasOffset = Canvas.offset;
+  var mx = (50 - canvasOffset.x) / canvasScale.x;
+  var my = (300 - canvasOffset.y) / canvasScale.y;
+
   Canvas.context.fillStyle = 'red';
   Canvas.context.fillRect(mx, my, 300 * canvasScale.x, 150 * canvasScale.y)
 }
