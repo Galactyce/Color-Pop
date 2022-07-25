4c4ee93073d7a587f57f709029d1e12f078898c4
Galactyce <elias.c.geurts@gmail.com>
2022-07-24 22:29:47 -0500

    Commit

--- BackButton.js
+++ BackButton.js
@@ -14,7 +14,7 @@ BackButton.prototype.draw = function() {
   Canvas.drawImage(this.sprite, this.position, 0, this.origin, 0.5);
   Canvas.drawText(
     "Back To Home",
-    new Vector2(this.position.x + 10, this.position.y + 10),
+    new Vector2((this.position.x - this.origin.x) + 30, (this.position.y - this.origin.y) + 30),
     'black',
     'top',
     'Comic Sans',
