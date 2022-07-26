63ea66e9ff58f128f97fa82ec805e7413c0f222c
Galactyce <elias.c.geurts@gmail.com>
2022-07-25 20:35:07 -0500

    Commit

--- PainterGameWorld.js
+++ PainterGameWorld.js
@@ -139,7 +139,7 @@ GameWorld.prototype.checkCookies = function () {
     var cname = ccookie[i].split("=");
 
     for (var k = 0; k < this.shopItems.length; k++) {
-       console.log("ccookie:" + ccookie[k].trim() + ',' + 'item'+ k + '=' + this.shopItems[k].item)
+       console.log("ccookie:" + ccookie[k].trim() + ',' + 'item'+ (k + 1) + '=' + this.shopItems[k + 1].item)
       if (ccookie[k].trim() == "item" + k + "=" + this.shopItems[k].item) {
         
         this.specialtiesOwned.push(this.shopItems[k].item);
