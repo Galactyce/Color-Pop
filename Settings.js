c457e8a37f2c4ae422e9ae1f5bd9158727c8546e
Galactyce <elias.c.geurts@gmail.com>
2022-07-26 22:17:02 -0500

    Commit

--- Settings.js
+++ Settings.js
@@ -2,10 +2,10 @@ function SettingsButton(position) {
   alert()
   this.sprite = sprites.extras['settings_icon'].normal;
   this.position = position;
-  this.origin = new Vector2(this.sprite.width / 2, this.sprite.height / 2);
+ 
   this.rect = new Rectangle(
-    position.x - this.origin.x / 2,
-    position.y - this.origin.y / 2,
+    position.x, 
+    position.y,
     this.sprite.width * 0.5,
     this.sprite.height * 0.5
   );
@@ -13,12 +13,7 @@ function SettingsButton(position) {
 
 SettingsButton.prototype.draw = function() {
   Canvas.drawImage(this.sprite, this.position, 0, this.origin, 0.3);
-  this.rect = new Rectangle(
-    position.x - this.origin.x / 2,
-    position.y - this.origin.y / 2,
-    this.sprite.width * 0.5,
-    this.sprite.height * 0.5
-  );
+
 }
 
 SettingsButton.prototype.update = function() {
